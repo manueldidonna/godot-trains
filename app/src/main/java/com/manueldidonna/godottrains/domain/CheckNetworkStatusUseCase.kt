789bd317aa334dd7f710aebd5f4fe2165c96039f
manueldidonna <manueldidonnadev@gmail.com>
2021-12-27 02:58:32 +0100

    Fix CheckNetworkStatusUseCase.kt bug when the user is connected to both wifi and cellular

--- app/src/main/java/com/manueldidonna/godottrains/domain/CheckNetworkStatusUseCase.kt
+++ app/src/main/java/com/manueldidonna/godottrains/domain/CheckNetworkStatusUseCase.kt
@@ -5,42 +5,43 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
+import android.util.Log
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.channelFlow
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
 
+@SuppressLint("MissingPermission")
 class CheckNetworkStatusUseCase @Inject constructor(
     private val connectivityManager: ConnectivityManager
 ) {
 
-    @SuppressLint("MissingPermission")
     @OptIn(ExperimentalCoroutinesApi::class)
     fun isConnectedToNetwork(): Flow<Boolean> {
-        return callbackFlow {
-            send(getCurrentNetworkStatus())
-            val networkStatusCallback = object : ConnectivityManager.NetworkCallback() {
-                override fun onUnavailable() {
-                    trySend(false)
-                }
+        return combine(
+            isNetworkTransportAvailable(NetworkCapabilities.TRANSPORT_CELLULAR),
+            isNetworkTransportAvailable(NetworkCapabilities.TRANSPORT_WIFI),
+        ) { cellular, wifi -> cellular || wifi }
+    }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun isNetworkTransportAvailable(transportType: Int): Flow<Boolean> {
+        return callbackFlow {
+            send(networkCapabilities?.hasTransport(transportType) ?: false)
+            val networkStatusCallback = object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: Network) {
+                    Log.d("Network Status", "$transportType available")
                     trySend(true)
                 }
 
                 override fun onLost(network: Network) {
+                    Log.d("Network Status", "$transportType lost")
                     trySend(false)
                 }
             }
-
-            val request = NetworkRequest.Builder()
-                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                .build()
-
+            val request = NetworkRequest.Builder().addTransportType(transportType).build()
             connectivityManager.registerNetworkCallback(request, networkStatusCallback)
 
             awaitClose {
@@ -49,15 +50,6 @@ class CheckNetworkStatusUseCase @Inject constructor(
         }
     }
 
-    @SuppressLint("MissingPermission")
-    private fun getCurrentNetworkStatus(): Boolean {
-        val networkCapabilities = connectivityManager.activeNetwork ?: return false
-        val actNw = connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
-        return when {
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            else -> false
-        }
-    }
+    private inline val networkCapabilities: NetworkCapabilities?
+        get() = connectivityManager.activeNetwork?.let(connectivityManager::getNetworkCapabilities)
 }
